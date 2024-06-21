cb1344d7be0a920bd0ed54b025211e5e224ff79a
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-06-21 10:11:26 +0200

    add EasyRepo

---
+++ src/main/java/repo/EasyRepo.java
@@ -0,0 +1,13 @@
+package repo;
+
+import model.Gruppe;
+import model.Tour;
+import model.Wanderer;
+
+import java.util.Map;
+
+public class EasyRepo {
+    private Map<String, Tour> touren;
+    private Map<String, Wanderer> wandererListe;
+    private Map<String, Gruppe> gruppen;
+}
