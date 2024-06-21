f720c15d242680b7a9d3a07a9ae575208ecfcdd0
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-06-21 08:49:42 +0200

    add something

---
+++ src/main/java/org/example/assessment2_thorsten/Assessment2ThorstenApplication.java
@@ -0,0 +1,13 @@
+package org.example.assessment2_thorsten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assessment2ThorstenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assessment2ThorstenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/assessment2_thorsten/Assessment2ThorstenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assessment2_thorsten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assessment2ThorstenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
