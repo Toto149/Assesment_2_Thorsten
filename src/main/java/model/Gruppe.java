f2fbb30c596390a10365fc139c60b8d69c600dd0
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-06-21 11:17:55 +0200

    add addWanderer(),addMehrererWanderer, entferneWanderer().

--- src/main/java/model/Gruppe.java
+++ src/main/java/model/Gruppe.java
@@ -1,14 +1,34 @@
 package model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Data
 @Document(collection = "gruppen")
 public class Gruppe {
     @Id
     private String id;
     private List<Wanderer> mitglieder;
-    private List<Tour> tour;
+
+    public boolean addWanderer(Wanderer wanderer){
+        return mitglieder.add(wanderer);
+    }
+
+    public boolean addMehrererWanderer(List<Wanderer> wandererListe){
+        return mitglieder.addAll(wandererListe);
+    }
+
+
+    public boolean entferneWanderer(String id){
+        Wanderer wandererDerEntferntWird = mitglieder
+                .stream()
+                .filter(wanderer -> wanderer.getId().equals(id))
+                .toList()
+                .getFirst();
+        return mitglieder.remove(wandererDerEntferntWird);
+    }
+
 }
