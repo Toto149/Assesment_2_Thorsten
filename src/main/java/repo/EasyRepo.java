a1e6c1aade71bb6222be5080e9f2c06ab56f216f
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-06-21 10:55:51 +0200

    add getAllWanderer, getAllTouren, getWandererById, addWanderer

--- src/main/java/repo/EasyRepo.java
+++ src/main/java/repo/EasyRepo.java
@@ -1,13 +1,43 @@
 package repo;
 
+import com.mongodb.client.model.MergeOptions;
+import lombok.Data;
 import model.Gruppe;
 import model.Tour;
 import model.Wanderer;
 
+import java.util.List;
 import java.util.Map;
 
+@Data
 public class EasyRepo {
-    private Map<String, Tour> touren;
-    private Map<String, Wanderer> wandererListe;
-    private Map<String, Gruppe> gruppen;
+    private Map<String, Tour> tourenMap;
+    private Map<String, Wanderer> wandererMap;
+    private Map<String, Gruppe> gruppenMap;
+
+
+    public boolean addWanderer(String id, Wanderer wanderer){
+       if(wanderer.equals(wandererMap.put(id, wanderer))){
+           return true;
+       }else {
+           return false;
+       }
+
+    }
+
+
+    public boolean addGruppeZurTour(String gruppenId, String tourId){
+        Tour tour = tourenMap.get(tourId);
+        Gruppe gruppe = gruppenMap.get(gruppenId);
+
+        return tour.addGruppe(gruppe);
+    }
+
+    public List<Tour> getTourenDieNichtAusgebuchtSind(){
+        return tourenMap
+                .values()
+                .stream()
+                .filter(tour -> !tour.isAusgebucht())
+                .toList();
+    }
 }
